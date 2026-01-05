90f7a74a98ef83e15b7913ae3803552899c0a100
ElijDavis <elijahadavis@hotmail.com>
2026-01-05 03:57:04 -0500

    Updated main: main function now supports supabase integration with url and anon

--- notify_flutter/lib/main.dart
+++ notify_flutter/lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 import 'dart:io'; // Add this for Platform check
 import 'package:sqflite_common_ffi/sqflite_ffi.dart'; // Add this
 import 'screens/home_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: 'https://zsqofefvkcsvalcigyti.supabase.co',
+    anonKey: 'sb_publishable_5mYGQFeNIdxVdkPHBvEHFg_RLOoAQTj',
+  );
+
   // 1. Check if we are on Desktop (Windows, Linux, or Mac)
   if (Platform.isWindows || Platform.isLinux) {
     // 2. Initialize the database factory for desktop
