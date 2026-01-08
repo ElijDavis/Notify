176268554f468b62550d9527a166857fc6ecd7a4
ElijDavis <elijahadavis@hotmail.com>
2026-01-08 18:02:25 -0500

    Dependency fix: fixed android dependency version issues

--- notify_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ notify_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import path_provider_foundation
 import record_macos
 import share_plus
 import shared_preferences_foundation
-import speech_to_text_macos
+import speech_to_text
 import sqflite_darwin
 import sqlite3_flutter_libs
 import url_launcher_macos
@@ -25,7 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
